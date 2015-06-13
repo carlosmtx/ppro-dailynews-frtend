ac8a2b01889ce7e3eefd7a180cd0d81095cc4473
cteixeiraVO <cteixeira@ventureoak.com>
2015-06-13 14:24:14 +0100

    [Fixed] NavBar url

--- js/controllers/HeaderCtrl.js
+++ js/controllers/HeaderCtrl.js
@@ -4,9 +4,16 @@ var newsAppController = angular.module('angularControllers');
 newsAppController.controller('HeaderCtrl', ['$scope','$routeParams','auth',
     function($scope,$routeParams,auth) {
         auth.register(function(){
-            console.log("Adoro ser callback");
             $scope.user = {};
             $scope.user.username = auth.getUser().username;
         });
+
+        $scope.$on('$routeChangeSuccess', function() {
+            if($routeParams.username){
+                $scope.user = {};
+                $scope.user.username = $routeParams.username;
+            }
+        });
+
     }
 ]);
